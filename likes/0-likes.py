a793f05b6c62d9257a022c8eff77b64a48a19a5f
Jonathan McFadden <mcfaddja@uw.edu>
2018-05-29 15:33:41 -0700

    added code to read and process the 'likes'

--- likes/0-likes.py
+++ likes/0-likes.py
@@ -24,3 +24,40 @@ from sklearn.externals import joblib
 import time
 
 
+likes = pd.read_csv("/home/jamster/data/training/relation/relation.csv")
+
+likesUIDs = likes.ix[:,1].values
+likesLIDs = likes.ix[:,2].values
+lsLikesUIDs = likesUIDs.tolist()
+lsLikesLIDs = likesLIDs.tolist()
+setLikesUIDs = set(lsLikesUIDs)
+setLikesLIDs = set(lsLikesLIDs)
+unqLikesUIDs = (list(setLikesUIDs))
+unqLikesLIDs = (list(setLikesLIDs))
+allLikesLS = [lsLikesUIDs, [str(x) for x in lsLikesLIDs]]
+allLikesLS = list(map(list, zip(*allLikesLS)))
+
+aDictLikes2 = {}
+for aUID in unqLikesUIDs:
+	aDictLikes2[aUID]=[]
+
+for row in allLikesLS:
+	aDictLikes2[row[0]].append(row[1])
+
+combDICT = {}
+for uid in unqLikesUIDs:
+	tmpDICT={}
+	tmpLS = aDictLikes2[uid]
+	for row in tmpLS:
+		tmpDICT[str(row)]=1
+	combDICT[uid]=tmpDICT
+
+tryTHIS=[]
+for uid in unqLikesUIDs:
+	tryTHIS.append(combDICT[uid])
+
+
+v = DictVectorizer()
+likesMAT=v.fit_transform(tryTHIS)
+
+
