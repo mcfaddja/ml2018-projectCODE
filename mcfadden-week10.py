58d0b6106dda2e5a22f53e5a334515b607f8d728
Jonathan McFadden <mcfaddja@uw.edu>
2018-05-31 13:30:01 -0700

    initial VM run python code

---
+++ mcfadden-week10.py
@@ -0,0 +1,14 @@
+import random
+import numpy as np
+import pandas as pd
+import os
+import sys
+import xml.etree.ElementTree as ET
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
+
+
+path ="/data/training/"
+
