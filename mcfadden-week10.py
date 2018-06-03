22b3131f54c4506475b2072d7230cb1dc8bd5027
Jonathan McFadden <mcfaddja@uw.edu>
2018-06-03 16:32:24 -0700

    cleaning up week10 running code

--- mcfadden-week10.py
+++ mcfadden-week10.py
@@ -1,3 +1,12 @@
+# course: TCSS555
+# ML in Python, Term Project
+# date: 06/01/2018
+# name: Jonathan McFadden
+# description: Code to run the models of MyFacebook data which were generated from the 'LIKES' data
+
+
+# Import initial required libraries
+###############
 import random
 import numpy as np
 import pandas as pd
@@ -10,25 +19,40 @@ from sklearn.metrics import confusion_matrix
 
 
 
+# Set file/directory paths
+###############
 pathLIKEs = "/data/training/relation"
 pathPROFs = "/data/training/profile"
-test_pathLIKEs = sys.argv[1]+"/relation"
-test_pathPROFs = sys.argv[1]+"/profile"
-# test_pathLIKEs = "/data/public-test-data/relation"
-# test_pathPROFs = "/data/public-test-data/profile"
+# test_pathLIKEs = sys.argv[1]+"/relation"
+# test_pathPROFs = sys.argv[1]+"/profile"
+test_pathLIKEs = "/data/public-test-data/relation"
+test_pathPROFs = "/data/public-test-data/profile"
 
+
+
+# Import the 'LIKES' from the training dataset and process
+###############
 dfLIKES = pd.read_csv(pathLIKEs + "/relation.csv")
+
+# Extact individual columns and convert to lists
 likesUIDs = dfLIKES.ix[:,1].values
 likesLIDs = dfLIKES.ix[:,2].values
 lsLikesUIDs = likesUIDs.tolist()
 lsLikesLIDs = likesLIDs.tolist()
+
+# Convert columns to sets
 setLikesUIDs = set(lsLikesUIDs)
 setLikesLIDs = set(lsLikesLIDs)
+
+# Convert columns to list of unique items
 unqLikesUIDs = (list(setLikesUIDs))
 unqLikesLIDs = (list(setLikesLIDs))
+
+# Get list of all User IDs (UIDs) paried with the Like IDs (LIDs) of the posts the user has liked
 allLikesLS = [lsLikesUIDs, [str(x) for x in lsLikesLIDs]]
 allLikesLS = list(map(list, zip(*allLikesLS)))
 
+# Convert list of UID and LID pairs into a dictionary indexed by UIDs
 aDictLikes2 = {}
 for aUID in unqLikesUIDs:
 	aDictLikes2[aUID]=[]
@@ -36,14 +60,30 @@ for aUID in unqLikesUIDs:
 for row in allLikesLS:
 	aDictLikes2[row[0]].append(row[1])
 
-
+import collections
 combDICT = {}
 for uid in unqLikesUIDs:
 	tmpDICT={}
 	tmpLS = aDictLikes2[uid]
+
 	for row in tmpLS:
 		tmpDICT[str(row)]=1
+    
 	combDICT[uid]=tmpDICT
+
+# for uid in unqLikesUIDs:
+#     tmpLS = aDictLikes2[uid]
+#     for item, count in collections.Counter(tmpLS).items():
+#         if count > 1: print(item)
+    
+# print(combDICT[uid])
+
+# import collections
+# print(collections.Counter(tmpLS).items())
+# print([item for item, count in collections.Counter(tmpLS).items() if count > 1])
+# for item, count in collections.Counter(tmpLS).items():
+#     if count <= 1:
+#             print(item)
 
 
 tryTHIS=[]
@@ -57,32 +97,9 @@ print("done with processing likes matrix from training data")
 
 
 
-# print(len(unqLikesUIDs))
-# print(len(unqLikesLIDs))
-# print(type(unqLikesUIDs))
-# print(type(unqLikesLIDs))
-
-
-
-
 test_dfLIKES = pd.read_csv(test_pathLIKEs + "/relation.csv")
-# print(test_dfLIKES.shape)
-# test_likesUIDs = test_dfLIKES.ix[:,1].values
-# test_likesLIDs = test_dfLIKES.ix[:,2].values
-# test_lsLikesUIDs = test_likesUIDs.tolist()
-# test_lsLikesLIDs = test_likesLIDs.tolist()
-# test_setLikesUIDs = set(test_lsLikesUIDs)
-# test_setLikesLIDs = set(test_lsLikesLIDs)
-# test_unqLikesUIDs = (list(test_setLikesUIDs))
-# test_unqLikesLIDs = (list(test_setLikesLIDs))
-# test_allLikesLS = [test_lsLikesUIDs, [str(x) for x in test_lsLikesLIDs]]
-# test_allLikesLS = list(map(list, zip(*test_allLikesLS)))
-
-# print(len(test_unqLikesUIDs))
-
 
 test_dfLIKES = test_dfLIKES[ test_dfLIKES['like_id'].isin(unqLikesLIDs)]
-# print(test_dfLIKES.shape)
 
 test_likesUIDs = test_dfLIKES.ix[:,1].values
 test_likesLIDs = test_dfLIKES.ix[:,2].values
@@ -95,32 +112,16 @@ test_unqLikesLIDs = (list(test_setLikesLIDs))
 test_allLikesLS = [test_lsLikesUIDs, [str(x) for x in test_lsLikesLIDs]]
 test_allLikesLS = list(map(list, zip(*test_allLikesLS)))
 
-# print(test_dfLIKES['userid', 'like_id'])
-# print(test_dfLIKES)
-# print(len(test_unqLikesUIDs))
-
 # test_dfLIKES.to_csv("/Users/jamster/Desktop/test.csv")
-
-# print(unqLikesLIDs[1])
-# print(test_lsLikesLIDs.index(unqLikesLIDs[1]))
-# print(test_lsLikesLIDs[test_lsLikesLIDs.index(unqLikesLIDs[1])])
-
-# print(len(test_unqLikesUIDs))
 
 test_aDictLikes2 = {}
 for aUID in test_unqLikesUIDs:
 	test_aDictLikes2[aUID]=[]
 
-# print(test_aDictLikes2)
-
-
 for row in test_allLikesLS:
 	test_aDictLikes2[row[0]].append(row[1])
 
 # test_aDictLikes2["dummy"] = unqLikesLIDs
-
-
-# print(test_aDictLikes2)
 
 
 test_combDICT = {}
@@ -136,9 +137,6 @@ test_tryTHIS=[]
 for uid in test_unqLikesUIDs:
 	test_tryTHIS.append(test_combDICT[uid])
 
-# print(test_combDICT[uid])
-# print(type(test_combDICT[uid]))
-# print(type(unqLikesLIDs))
 
 dummyDICT = {}
 for row in unqLikesLIDs:
@@ -146,26 +144,13 @@ for row in unqLikesLIDs:
 
 test_tryTHIS.append(dummyDICT)
 test_unqLikesUIDs.append("dummy")
-# print(len(test_unqLikesUIDs))
-# print(test_unqLikesUIDs)
 
 test_v = DictVectorizer()
 test_likesMAT=test_v.fit_transform(test_tryTHIS)
 print("done with processing likes matrix from test data")
 
-tmp0 = test_likesMAT * np.ones((test_likesMAT.shape[1], 1))
-# print(tmp0)
+# tmp0 = test_likesMAT * np.ones((test_likesMAT.shape[1], 1))
 
-# print(test_likesMAT.shape)
-# print(test_likesMAT[317,:])
-# print(test_likesMAT[318,:])
-
-# test_likesMATo = np.delete(test_likesMAT, 318, 0)
-# test_likesMAT.row_del(318)
-# print(test_likesMAT[317,:])
-# print(test_likesMAT[318,:])
-# print(test_likesMATo[317,:])
-# print(test_likesMATo[318,:])
 
 
 dfPROFS = pd.read_csv(pathPROFs + "/profile.csv")
@@ -208,27 +193,17 @@ test_dfPROFS = pd.read_csv(test_pathPROFs + "/profile.csv")
 test_profiles = test_dfPROFS.ix[:,1].values.copy()
 test_profilesLSo = test_profiles.tolist().copy()
 
-# print(test_profilesLSo)
-# print(len(test_profilesLSo))
-
 test_profilesLS=[]
 for row in test_profilesLSo:
 	tmpLS=row
 	test_profilesLS.append(tmpLS)
 
 test_profilesLS.append("dummy")
-# print(len(test_profilesLS))
-
-# print(test_profilesLS)
 
 test_profsTOlikes=[]
 for i in range(len(test_unqLikesUIDs)):
 	test_profsTOlikes.append([])
 
-# # print(len(test_profsTOlikes))
-# # print(test_profsTOlikes)
-
-# # print(test_unqLikesUIDs)
 
 notINlist = []
 for row in test_profilesLS:
@@ -238,26 +213,15 @@ for row in test_profilesLS:
         notINlist.append(row)
     test_profsTOlikes[tmpIND]=row
 
-# # print(len(test_profsTOlikes))
-# # print(test_profsTOlikes)
-# print(len(notINlist))
-# print(notINlist)
 
 test_profsTOlikes1=list(map(list, zip(*test_profsTOlikes)))
 
 print("finished processing profile matrix from test data")
 
-# # print(notINlist)
 
-# print(len(test_profilesLS))
-# i=0
-# for prof in test_profilesLS:
-#     if prof in test_unqLikesUIDs:
-#         print(test_unqLikesUIDs.index(prof))
-#         i+=1
 
-# print(i)
-
+# Import Keras Libraries
+###############
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
@@ -266,53 +230,57 @@ from keras.layers import Input, Dense, Dropout, Activation
 from keras.optimizers import RMSprop, Adam, Nadam, Adamax
 from keras.models import model_from_json
 
-# json_file = open("/Users/jamster/mcfaddja-models/AGES-Keras.json", 'r')
+
+# Import Keras AGES model from file
+###############
 json_file = open("/home/itadmin/mcfaddja-models/AGES-Keras.json", 'r')
+# json_file = open("/Users/jamster/mcfaddja-models/AGES-Keras.json", 'r')
 agesMODEL_json = json_file.read()
 json_file.close()
 agesMODEL = model_from_json(agesMODEL_json)
-# agesMODEL.load_weights("/Users/jamster/mcfaddja-models/AGES-Keras.h5")
 agesMODEL.load_weights("/home/itadmin/mcfaddja-models/AGES-Keras.h5")
+# agesMODEL.load_weights("/Users/jamster/mcfaddja-models/AGES-Keras.h5")
+
 print("loaded AGES model from disk")
 
+
+# Complile Keras AGES model and predict
+###############
 agesMODEL.compile(optimizer='adam',
 				loss='categorical_crossentropy',
 				metrics=['accuracy', 'mse'])
 
 pred_ages = agesMODEL.predict(test_likesMAT)
+
 print("AGES model has made predictions")
 
-# print(pred_ages)
-# print(pred_ages[1])
-# print(type(pred_ages))
-# print(type(pred_ages[1]))
 
-# i = 0
-# for row in pred_ages:
-#     tmpROW = np.around(row, decimals=0)
-#     pred_ages[i]=tmpROW
-#     i+=1
-
+# Process the predictions of the Keras AGES model
+###############
 proc_ages = []
 for row in pred_ages:
     tmpGRP = np.argmax(row)
     tmpGRP+=1
     proc_ages.append(tmpGRP)
 
-# print(proc_ages)
 print("predictions by AGES model have been processed")
 
 
 
-# json_file = open("/Users/jamster/mcfaddja-models/SEXS-Keras.json", 'r')
+# Import Keras SEXS model from file
+###############
 json_file = open("/home/itadmin/mcfaddja-models/SEXS-Keras.json", 'r')
+# json_file = open("/Users/jamster/mcfaddja-models/SEXS-Keras.json", 'r')
 sexsMODEL_json = json_file.read()
 json_file.close()
 sexsMODEL = model_from_json(sexsMODEL_json)
-# sexsMODEL.load_weights("/Users/jamster/mcfaddja-models/SEXS-Keras.h5")
 sexsMODEL.load_weights("/home/itadmin/mcfaddja-models/SEXS-Keras.h5")
+# sexsMODEL.load_weights("/Users/jamster/mcfaddja-models/SEXS-Keras.h5")
+
 print("loaded SEXS model from disk")
 
+# Complile Keras SEXS model and predict
+###############
 sexsMODEL.compile(optimizer='adam',
 				loss='binary_crossentropy',
 				metrics=['accuracy', 'mse'])
@@ -320,11 +288,9 @@ sexsMODEL.compile(optimizer='adam',
 pred_sexs = sexsMODEL.predict(test_likesMAT)
 print("SEXS model has made predictions")
 
-# print(pred_sexs)
-# print(pred_sexs[1])
-# print(type(pred_sexs))
-# print(type(pred_sexs[1]))
 
+# Process the predictions of the Keras SEXS model
+###############
 proc_sexs = []
 for row in pred_sexs:
     tmpSEX = np.around(row, decimals=0)
@@ -386,16 +352,16 @@ print("predictions by SEXS model have been processed")
 
 
 
-print("yay!")
 
 
 
 
-
-
-
+# Create XML files for users with LIKES data in the test dataset
+###############
 def create_xmlA(i,user):
     root = ET.Element("user")
+
+    # AGES
     if proc_ages[i]==1:
 	    root.set("age_group","xx-24")
     elif proc_ages[i]==2:
@@ -405,27 +371,38 @@ def create_xmlA(i,user):
     else:
         root.set("age_group","50-xx")
 
+    # GENDER/SEXS
     if proc_sexs[i]==0:
         root.set("gender","male")
     else:
         root.set("gender","female")
 
+    # USER ID
     root.set("id", test_profsTOlikes[i])
 
+    # EXTs
     root.set("extrovert","3.49")
 
+    # NEUs
     root.set("neurotic","2.73")
 
+    # AGRs
     root.set("agreeable","3.58")
 
+    # CONs
     root.set("conscientious","3.45")
 
+    # OPEs
     root.set("open", "3.91")
+
+    # Create XML Tree
     tree = ET.ElementTree(root)
+
+    # Write XML Tree to file
     tree.write(sys.argv[2]+"/"+str(user)+".xml")
     # tree.write("/Users/jamster/output/"+str(user)+".xml")
 
-    # return 0
+
 
 # def create_xmlA(i,user):
 #     root = ET.Element("user")
@@ -458,9 +435,10 @@ def create_xmlA(i,user):
 #     # tree.write(sys.argv[2]+"/"+str(user)+".xml")
 #     tree.write("/Users/jamster/output/"+str(user)+".xml")
 
-    # return 0
     
 
+# Create XML files for users without LIKES data in the test dataset
+###############
 def create_xmlB(user):
     root = ET.Element("user")
     root.set("age_group","xx-24")
@@ -471,25 +449,18 @@ def create_xmlB(user):
     root.set("agreeable","3.58")
     root.set("conscientious","3.45")
     root.set("open","3.91")
+
+    # Create XML Tree
     tree = ET.ElementTree(root)
+
+    # Write XML Tree to file
     tree.write(sys.argv[2]+"/"+str(user)+".xml")
     # tree.write("/Users/jamster/output/"+str(user)+".xml")
 
 
-# i=0
-# print(test_profilesLS)
-# for user in test_profilesLS:
-#     ind = test_profilesLS.index(user)
-#     print(ind)
-#     create_xmlA(ind, user)
-#     # i+=1
-#     # print(i)
 
-# print(notINlist)
-# for user in notINlist:
-#     ind  = notINlist.index(user)
-#     create_xmlB(ind, user)
-
+# Generate the XML files from the predicted data
+###############
 for user in test_profilesLS:
     if user != "dummy" and user in test_profsTOlikes:
         ind = test_profsTOlikes.index(user)
@@ -504,21 +475,3 @@ for user in test_profilesLS:
             create_xmlB(user)
 
 
-
-
-# # NB->age and gender.
-# df = pd.read_csv("/data/training/profile/profile.csv")
-# df['text']=''
-# for i in range(0,len(df)):
-# 	#print(row['userid']+info.get(row['userid']))
-#     print(df.loc[i,'userid']+info.get(df['userid']))
-# 	df.loc[i,'text'] = info.get(df.loc[i,'userid'])
-# 	age = int(df.loc[i,'age'])
-# 	if(age<=24):
-# 		df.loc[i,'age']="xx-24"
-# 	elif(age>=25 and age<=34):
-# 		df.loc[i,'age']="25-34"
-# 	elif(age>=35 and age<=49):
-# 		df.loc[i,'age']="35-49"
-# 	else:
-# 		df.loc[i,'age']="50-xx"
